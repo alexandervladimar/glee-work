c47b48016078efddca9f4fb2667b124908b22f4d
alexandervladimar <enim2036@gmail.com>
2021-06-16 18:34:25 +0300

    added blog, added product-list, added contact

--- app/js/main.js
+++ app/js/main.js
@@ -52,3 +52,32 @@ $('.company__items').slick({
         },
     ],
 });
+$('.product-related__items').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    prevArrow: '<button type="button" class="product-related__btn product-related__btn--prev"><img src="images/left-arrow.svg" alt="arrow" width="22" height="9"></button>',
+    nextArrow: '<button type="button" class="product-related__btn product-related__btn--next"><img src="images/right-arrow.svg" alt="arrow" width="22" height="9"></button>',
+    responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
+$('.product-details__star').rateYo({
+    rating: 5,
+    starWidth: '18px',
+    starHeight: '18px',
+    normalFill: '#d6d6d6',
+    ratedFill: '#fc0',
+});
